cbd30a3f1bdd94f6d618be47e646c86712726a2b
NicoooM <nmarsan33000@gmail.com>
2024-04-12 14:57:22 +0200

    feat: exercise 9

--- src/main/java/fr/hetic/Calculateur.java
+++ src/main/java/fr/hetic/Calculateur.java
@@ -1,17 +1,23 @@
 package fr.hetic;
 
 import java.io.*;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Calculateur {
     public static void main(String[] args) {
-        File folder = new File("./fr/hetic/operations");
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")) {
+            File folder = new File("operations");
 
-        if (!folder.exists()) {
-            System.out.println("Directory does not exist");
-            return;
+            if (!folder.exists()) {
+                System.out.println("Directory does not exist");
+                return;
+            }
+
+            FileProcessor processor = context.getBean("fileProcessor", FileProcessor.class);
+
+            processor.processDirectory(folder);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        FileProcessor processor = new FileProcessor();
-        processor.processDirectory(folder);
     }
 }
